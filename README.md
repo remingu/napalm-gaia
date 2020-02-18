b496df7442cad529e629ca07a5720e1981a825ff
remingu <neodyme@geekpit.org>
2020-02-18 19:29:58 +0100

    added function bodies

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # napalm-gaia
+
+driver-plugin for NAPALM (Network Automation and Programmability Abstraction Layer with Multivendor support) python library 
--- napalm-gaiaos/gaiaos.py
+++ napalm-gaiaos/gaiaos.py
@@ -18,6 +18,24 @@ class GaiaOSDriver(NetworkDriver):
     def close(self):
         pass
 
+    def get_users(self):
+        pass
+
+    def get_arp_table(self, vrf=""):
+        pass
+
+    def get_config(self, retrieve="all", full=False):
+        pass
+
+    def get_facts(self):
+        pass
+
+    def get_interfaces(self):
+        pass
+
+    def get_interfaces_ip(self):
+        pass
+
 
 if __name__ == '__main__':
     pass
