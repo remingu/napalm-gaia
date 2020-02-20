cfe43dd91078869f741c3f32ea58b00f5ca95297
remingu <neodyme@geekpit.org>
2020-02-20 23:32:54 +0100

    fixed setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-    name='napalm_gaia',
-    version='',
-    packages=['napalm_gaiaos', 'napalm-gaiaos.helper'],
-    url='',
-    license='',
+    name='napalm-gaia',
+    version='0.0.1',
+    packages=['napalm_gaiaos', 'napalm_gaiaos.helper'],
+    url='https://github.com/remingu/napalm-gaia/',
+    license='Apache License 2.0',
     author='remingu, mbtathcx',
     author_email='',
     description=''
