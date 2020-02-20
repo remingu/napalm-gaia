2a3a13a34c99d01312289c35eedfc1925e12960c
remingu <52041034+remingu@users.noreply.github.com>
2020-02-20 23:30:45 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='napalm_gaia',
     version='',
-    packages=['napalm_gaiaos', 'napalm-gaiaos.helper'],
+    packages=['napalm_gaiaos', 'napalm_gaiaos.helper'],
     url='',
     license='',
     author='remingu, mbtathcx',
