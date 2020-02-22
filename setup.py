8ec3e691f7d52bcec048601652e0b9458fdf0c5c
remingu <neodyme@geekpit.org>
2020-02-22 17:31:48 +0100

    added func::napalm_gaiaos/gaiaos.send_expert_cmd()

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='napalm-gaia',
-    version='0.0.1b1',
+    version='0.0.1b2',
     packages=['napalm_gaiaos', 'napalm_gaiaos.helper'],
     url='',
     license='',
