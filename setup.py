cccd6b8f2dbe6291485377ee11a5c5f00804a783
remingu <neodyme@geekpit.org>
2020-02-23 15:13:47 +0100

    updated packages

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='napalm-gaia',
-    version='0.0.1b4-1',
+    version='0.0.1b5',
     packages=['napalm_gaiaos', 'napalm_gaiaos.helper'],
     url='',
     license='',
