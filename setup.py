757ef89705224dc6e064ee08baec2be566518326
remingu <neodyme@geekpit.org>
2020-02-24 10:26:52 +0100

    updated expert functions()

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='napalm-gaia',
-    version='0.0.1b8-1',
+    version='0.0.1b8-2',
     packages=['napalm_gaiaos', 'napalm_gaiaos.helper'],
     url='',
     license='',
