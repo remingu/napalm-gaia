55e6d614d7521b22c2c8516234e6a724887d0528
remingu <neodyme@geekpit.org>
2020-02-26 00:26:17 +0100

    added overwrite methods for basedriver

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ from distutils.core import setup
 
 setup(
     name='napalm-gaia',
-    version='0.0.1c0',
+    version='0.0.2rc1',
     packages=['napalm_gaiaos', 'napalm_gaiaos.helper'],
     url='',
     license='',
     author='remingu, mbtathcx',
     author_email='',
-    description=''
+    description='',
+    install_requires=[
+       "napalm >= 2.5.0"]
 )
