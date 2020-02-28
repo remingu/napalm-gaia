dc3ca6bf4262cff467577c332a9278dba924cac1
remingu <neodyme@geekpit.org>
2020-02-28 01:43:25 +0100

    -

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Daniel Schlifka(remingu)'
 author = 'Daniel Schlifka, mbtathcx'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.4'
+release = '0.0.5'
 
 
 # -- General configuration ---------------------------------------------------
